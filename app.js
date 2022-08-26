93ac5c18edaa5911b142504482c72ca0d368e3ad
onarman <onarmannn@gmail.com>
2022-08-26 21:31:45 +0300

    some fail

--- app.js
+++ app.js
@@ -3,35 +3,52 @@
 //*===========================================================
 
 
+const searchBar =document.getElementById("searchBar");
+const countriesDiv = document.querySelector(".countries")
+let data = [];
+console.log(searchBar);
+
 const fetchCountry = async(name) => {
     const url = `https://restcountries.com/v3.1/name/${name}`;
 
     try {
         const res = await fetch(url) // istek gönderdik.
-        if(!res.ok){
+        if(!res.ok){ 
             renderError(`Something went wrong: ${res.status}`)
              throw new Error()
-        }
+        } 
 
-        const data = await res.json()
+        data = await res.json() 
+        
         renderCountry(data[0])
+        console.log(data[0])
 
     } catch (error) {
         console.log(error)
     }
-
 }
 
-const renderError = (err) => {
+searchBar.addEventListener("keyup", (e) =>{
+    const searchStr = e.target.value.toLowerCase();
+    // console.log(searchStr)
+    const filteredCountries = data.filter((country)=> {
+        console.log(country);
+        return (
+            country.name.toString().toLowerCase().includes(searchStr) || 
+            country.capital.toString().toLowerCase().includes(searchStr)
+        )
+    })
+    console.log(filteredCountries)
+})
 
-    const countriesDiv = document.querySelector(".countries")
+
+const renderError = (err) => {
+   const countriesDiv = document.querySelector(".countries")
     countriesDiv.innerHTML = `
     <h1 class="text-danger">${err}</h1>
     <img src="./img/indir.png" alt="" />
     `
-    
 }
-
 const renderCountry = (country) => {
     console.log(country);
     const countriesDiv = document.querySelector(".countries")
@@ -52,7 +69,7 @@ const renderCountry = (country) => {
 
     countriesDiv.innerHTML += `
     <div class="card shadow-lg" style="width: 18rem;">
-    <img src="${svg}" class="card-img-top" alt="...">
+    <img src="${svg}" class="card-img-top" alt="flag">
     <div class="card-body">
       <h5 class="card-title">${common}</h5>
       <p class="card-text">${region}</p>
@@ -71,5 +88,5 @@ const renderCountry = (country) => {
     
 }
 
-fetchCountry("france")
-fetchCountry("belgium")
+// fetchCountry("france")
+
